a3b1252c51855dac52fe1b8f23df97e66c684267
T. Himmelstoss <9889638+tfkhim@users.noreply.github.com>
2020-02-08 20:02:02 +0100

    POC of XMPP notification with aioxmpp library

--- auto-backup.py
+++ auto-backup.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python
 
+import sys
+import asyncio
+import aioxmpp
+
+async def send_notification(sender, password, recipient, msgStr):
+    jid = jid = aioxmpp.JID.fromstr(sender)
+    sec_layer = aioxmpp.make_security_layer(password)
+    recipient_jid = aioxmpp.JID.fromstr(recipient)
+
+    client = aioxmpp.PresenceManagedClient(jid, sec_layer)
+
+    async with client.connected() as stream:
+        msg = aioxmpp.Message(to=recipient_jid, type_=aioxmpp.MessageType.CHAT)
+
+        msg.body[None] = msgStr
+
+        await client.send(msg)
+
 if __name__ == "__main__":
-    print("__main__")
+    msg = "XMPP Hello World!"
+
+    import getpass
+    password = getpass.getpass()
+
+    asyncio.run(send_notification(sys.argv[1], password, sys.argv[2], msg))
--- setup.py
+++ setup.py
@@ -5,5 +5,7 @@ from setuptools import setup, find_packages
 setup(
     name = "auto-backup",
     version = "0.1",
-    py_modules = ["auto-backup"]
+    py_modules = ["auto-backup"],
+
+    install_requires = ["aioxmpp>=0.10.5"]
 )
