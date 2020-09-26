0e6051709735af755c255b08131b9b6366179402
T. Himmelstoss <9889638+tfkhim@users.noreply.github.com>
2020-09-26 14:35:12 +0200

    Fix an incompatibility with older Python versions
    The functools.cached_property decorator was added in Python version 3.8. But
    we also want to support some older Python versions. So we try to import the
    cached_property decorator from functools first and if this fails we fall
    back to a third party library with an identical interface.

--- auto_backup/__init__.py
+++ auto_backup/__init__.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 
 import argparse
-from functools import cached_property
+
+try:
+    from functools import cached_property
+except ImportError:
+    from cached_property import cached_property
 
 import toml
 
